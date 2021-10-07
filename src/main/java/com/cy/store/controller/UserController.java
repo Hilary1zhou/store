df983effebabbf8df4bea83c5add2c0cd8ba8e2b
周欢 <Hilary.zhou@outlook.com>
2021-10-07 20:15:33 +0800

    上传头像

--- src/main/java/com/cy/store/controller/UserController.java
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -57,18 +57,41 @@ public class UserController extends BaseController {
         //响应成功和数据
         return new JsonResult<User>(200, data);
     }
+
     @RequestMapping("update_info")
-    public JsonResult<Void> updateInfo(HttpSession session,User user) {
+    public JsonResult<Void> updateInfo(HttpSession session, User user) {
         //调用业务对象执行用户修改资料
-        userService.updateInfoByUid(getUidFromSession(session),getUserNameFromSession(session),user);
+        userService.updateInfoByUid(getUidFromSession(session), getUserNameFromSession(session), user);
         //响应成功
         return new JsonResult<Void>(OK);
     }
 
-
-
+    //定义上传文件大小的上限
+    public static final int AVATAR_MAX_SIZE = 10 * 1024 * 1024;
     @RequestMapping("change_avatar")
     public JsonResult<String> changeAvatar(HttpSession session, @RequestParam("file") MultipartFile file) {
+        // 判断上传的文件是否为空
+        // 是：抛出异常
 
+        // 判断上传的文件大小是否超出限制值
+        // 是：抛出异常
+
+        // 判断上传的文件类型是否超出限制
+        // 是：抛出异常
+
+        // 获取当前项目的绝对磁盘路径
+        // 保存头像文件的文件夹
+
+        // 保存的头像文件的文件名
+
+        // 创建文件对象，表示保存的头像文件
+        // 执行保存头像文件
+        // 如果产生异常则抛出
+
+        // 头像路径
+        // 从Session中获取uid和username
+        // 将头像写入到数据库中
+
+        // 返回成功和头像路径
     }
 }
