189b9d5ba6ef74e69ad282668012eb29f63f5678
周欢 <Hilary.zhou@outlook.com>
2021-10-09 11:20:12 +0800

    上传头像

--- src/main/java/com/cy/store/controller/UserController.java
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cy.store.controller;
 
+import com.cy.store.controller.ex.*;
 import com.cy.store.entity.User;
 import com.cy.store.service.Impl.IUserServiceImpl;
 import com.cy.store.util.JsonResult;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author huan
@@ -66,32 +72,81 @@ public class UserController extends BaseController {
         return new JsonResult<Void>(OK);
     }
 
-    //定义上传文件大小的上限
+    /**
+     * 头像文件大小的上限值(10MB)
+     */
     public static final int AVATAR_MAX_SIZE = 10 * 1024 * 1024;
+
+    /**
+     * 允许上传的头像的文件类型
+     */
+    public static final List<String> AVATAR_TYPES = new ArrayList<>();
+
+    /** 初始化允许上传的头像的文件类型 */
+    static {
+        AVATAR_TYPES.add("image/jpeg");
+        AVATAR_TYPES.add("image/png");
+        AVATAR_TYPES.add("image/bmp");
+        AVATAR_TYPES.add("image/gif");
+    }
+
+
     @RequestMapping("change_avatar")
     public JsonResult<String> changeAvatar(HttpSession session, @RequestParam("file") MultipartFile file) {
+
         // 判断上传的文件是否为空
-        // 是：抛出异常
-
+        if (file.isEmpty()) {
+            throw new FileEmptyException("上传的头像文件不存在");
+        }
         // 判断上传的文件大小是否超出限制值
-        // 是：抛出异常
-
-        // 判断上传的文件类型是否超出限制
-        // 是：抛出异常
+        if (file.getSize() > AVATAR_MAX_SIZE) {
+            throw new FileSizeException("不允许上传超过" + (AVATAR_MAX_SIZE / 1024) + "kb的头像文件");
+        }
+        // 判断上传的文件类型是否超出限制(contains测试列表是否包含元素)
+        if (!AVATAR_TYPES.contains(file.getContentType())) {
+            throw new FileTypeException("不支持使用该类型的文件作为头像，允许的文件类型：\n" + AVATAR_TYPES);
+        }
 
         // 获取当前项目的绝对磁盘路径
-        // 保存头像文件的文件夹
+        File dir = new File(session.getServletContext().getRealPath("upload"));
 
+        //保存头像文件的文件夹
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
         // 保存的头像文件的文件名
+        String suffix = "";
+        String originalFilename = file.getOriginalFilename();
+        int index = originalFilename.lastIndexOf(".");
+        if (index > 0) {
+            //文件的后缀名
+            suffix = originalFilename.substring(index);
+
+        }
+        String fileName = UUID.randomUUID().toString() + suffix;
 
         // 创建文件对象，表示保存的头像文件
+        File dest = new File(dir, fileName);
         // 执行保存头像文件
-        // 如果产生异常则抛出
+        try {
+            file.transferTo(dest);
+        } catch (IllegalStateException e) {
+            // 抛出异常
+            throw new FileStateException("文件状态异常，可能文件已被移动或删除");
+        } catch (IOException e) {
+            // 抛出异常
+            throw new FileUploadIOException("上传文件时读写错误，请稍后重尝试");
+        }
 
         // 头像路径
+        String avatar = "/upload/" + fileName;
         // 从Session中获取uid和username
+        String username = getUserNameFromSession(session);
+        Integer uid = getUidFromSession(session);
         // 将头像写入到数据库中
-
+        userService.changeAvatar(uid, username, avatar);
         // 返回成功和头像路径
+        return new JsonResult<String> (OK,avatar);
+
     }
 }
